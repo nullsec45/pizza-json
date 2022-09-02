dffab1328d008eba02a8a8bf27be822be09b7cc4
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-02 11:58:07 +0700

    feat-content:fill content with data statis

--- latihan1.php
+++ latihan1.php
@@ -38,9 +38,10 @@
               <div class="card" style="width: 18rem;">
                     <img src="img/menu/american-favourite.jpg" class="card-img-top" alt="american-favourite">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
+                        <h5 class="card-title">American Favourite</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">Rp 60.000</h5>
+                        <a href="#" class="btn btn-primary">Buy</a>
                     </div>
               </div>
             </div>
