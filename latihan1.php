bc7c287af4fa54d394e950f7088b9fd6c980adcc
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-02 11:50:39 +0700

    feat-content:rename folder img/pizza to img/menu

--- latihan1.php
+++ latihan1.php
@@ -36,7 +36,7 @@
         <div class="row mt-4">
             <div class="col-md-4">
               <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="img/menu/american-favourite.jpg" class="card-img-top" alt="american-favourite">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
