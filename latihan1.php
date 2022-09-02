0918acbcb595f8831c0a76bd06af9e72f722d5b9
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-02 16:14:13 +0700

    change title file1.php and file2.html

--- latihan1.php
+++ latihan1.php
@@ -13,7 +13,7 @@ $menus=json_decode($data, true)["menu"];
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Pizza JSON</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
