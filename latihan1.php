6b28d45ed2e3cd5493b9a1a3fb85177a100b27b5
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-02 14:24:13 +0700

    delete script js jquery in file latihan1.php

--- latihan1.php
+++ latihan1.php
@@ -54,7 +54,6 @@ $menus=json_decode($data, true)["menu"];
             <?php endforeach; ?>
         </div>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
